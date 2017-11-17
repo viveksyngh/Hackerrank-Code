c45cd0aac15cd36d6d76ad5fa65dc0a1433ce32e
Ralic Lo <raliclo@gmail.com>
2017-11-16 22:42:48 -0800

    For oracle R11 +
    Simplify query

--- SQL/Select/Weather Observation Station 8.sql
+++ SQL/Select/Weather Observation Station 8.sql
@@ -18,8 +18,5 @@ Write a query to print the list of CITY that starts with vowels and ends at vowe
 
 --Solution
 
-SELECT CITY 
-FROM STATION 
-WHERE (CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%' ) AND
-      (CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u' );
+SELECT DISTINCT CITY FROM STATION WHERE REGEXP_LIKE (UPPER(city),'^[AEIOU].*[AEIOU]$');
 	  
